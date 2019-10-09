09cea97e6c9ca2bb2e4bf74489a625331722a015
frankcapodanno <francesco.capodanno@gmail.com>
2019-10-09 11:56:43 +0200

    Code Review, Count Letters with tests and Coverage Criteria

--- src/main/java/tudelft/countletters/CountLetters.java
+++ src/main/java/tudelft/countletters/CountLetters.java
@@ -2,22 +2,27 @@ package tudelft.countletters;
 
 public class CountLetters {
 
-    public int count(String str) {
-        int words = 0;
-        char last = ' ';
-        for(int i = 0; i < str.length(); i++) {
-            if(!Character.isLetter(str.charAt(i)) &&
-                    (last == 'r' || last == 's')) {
-                words++;
-            }
+	public int count(String str) {
+		int words = 0;
+		char last;
+		int lenght = str.length();
+		boolean atEnd = false;
+		char[] charArray = str.toCharArray();
+		for (int i = 0; i < lenght; i++) {
+			last = charArray[i];
+			int nextPos = i + 1;
+			if (nextPos >= lenght) {
+				nextPos = i;
+				atEnd = true;
+			}
 
-            last = str.charAt(i);
-        }
-
-        if(last == 'x' || last == 's')
-            words++;
-
-        return words;
-    }
-
+			if (Character.isLetter(last)) {
+				if ((last == 'r' || last == 's' || last == 'x')
+						&& ((atEnd) || (!Character.isLetter(charArray[nextPos])))) {
+					words++;
+				}
+			}
+		}
+		return words;
+	}
 }
--- src/test/java/tudelft/countletters/CountLettersTest.java
+++ src/test/java/tudelft/countletters/CountLettersTest.java
@@ -16,5 +16,23 @@ public class CountLettersTest {
         int words = new CountLetters().count("cats|dog");
         Assertions.assertEquals(1, words);
     }
+    
+    @Test
+    public void multipleMatchingRWords() {
+        int words = new CountLetters().count("door|car");
+        Assertions.assertEquals(2, words);
+    }
+    
+    @Test
+    public void multipleMatchingLastDoubleLetter() {
+        int words = new CountLetters().count("cox|loss");
+        Assertions.assertEquals(2, words);
+    }
+    
+    @Test
+    public void multipleMatchingDoubleLetter() {
+    	int words = new CountLetters().count("coxx|loss|cipss");
+    	Assertions.assertEquals(3, words);
+    }
 
 }
